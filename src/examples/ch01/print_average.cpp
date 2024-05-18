25209b7742eb64a2cd42f8f25e7e8b769c63acff
Graham Strickland <grahamhstrickland@gmail.com>
2024-05-18 18:00:41 +0200

    Fix some bugs in print_average example

--- src/examples/ch01/print_average.cpp
+++ src/examples/ch01/print_average.cpp
@@ -16,7 +16,7 @@ void print_average(float v1, float v2, int language)
         "La moyenne de {v1} et {v2} est {result}.\n",
         "El promedio de {v1} y {v2} es {result}.\n",
         "{result} corrisponde alla media di {v1} e {v2}.\n"};
-    print(formats[language], "v1"_a= v1, "v2"_a= v2,
+    print(formats[1], "v1"_a= v1, "v2"_a= v2,
             "result"_a= (v1+v2)/2.0f);
 }
 
@@ -27,3 +27,4 @@ int main(int argc, char* argv[])
 
     return EXIT_SUCCESS;
 }
+
