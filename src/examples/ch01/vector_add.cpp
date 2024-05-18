59ce1329bd79a06f3bbee771a6de8222768b0f31
Graham Strickland <grahamhstrickland@gmail.com>
2024-05-18 19:26:37 +0200

    Add sizeof operations to vector addition example

--- src/examples/ch01/vector_add.cpp
+++ src/examples/ch01/vector_add.cpp
@@ -9,24 +9,21 @@ void vector_add(unsigned size, const double v1[], const double v2[],
 
 int main(int argc, char* argv[])
 {
-    unsigned size = 5;
-    double v1[] = {1.0, 2.0, 3.0, 4.0, 5.0},
-           v2[] = {-1.0, -1.1, -1.2, -1.3, -1.4},
-           s[size];
+    double x[]= {2, 3, 4}, y[]= {4, 2, 0}, sum[3];
+    vector_add(sizeof x / sizeof x[0], x, y, sum);
 
-    vector_add(size, v1, v2, s);
-
-    std::cout << "v1 + v2 = [";
+    unsigned size = sizeof x / sizeof x[0];
+    std::cout << "x + y = [";
     for (unsigned i= 0; i < size; ++i)
-        std::cout << v1[i] << (i != size - 1 ? ", " : "]");
+        std::cout << x[i] << (i < size - 1 ? ", " : "]");
 
     std::cout << " + [";
     for (unsigned i= 0; i < size; ++i)
-        std::cout << v2[i] << (i != size - 1 ? ", " : "]");
+        std::cout << y[i] << (i < size - 1 ? ", " : "]");
 
     std::cout << " = [";
     for (unsigned i= 0; i < size; ++i)
-        std::cout << s[i] << (i != size - 1 ? ", " : "]");
+        std::cout << sum[i] << (i < size - 1 ? ", " : "]");
     std::cout << '\n';
 
     return EXIT_SUCCESS;
