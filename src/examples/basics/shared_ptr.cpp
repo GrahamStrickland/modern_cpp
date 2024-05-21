d39b9728fa86fc3014088de3f360686ef9f5d94e
Graham Strickland <grahamhstrickland@gmail.com>
2024-05-21 18:50:01 +0200

    Add shared_ptr example

---
+++ src/examples/basics/shared_ptr.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include <iostream>
+
+std::shared_ptr<double> f()
+{
+    std::shared_ptr<double> p1{new double};
+    std::shared_ptr<double> p2{new double}, p3= p1;
+    std::cout << "p3.use_count() = " << p3.use_count() << std::endl;
+    return p3;
+}
+
+int main(int argc, char* argv[])
+{
+    std::shared_ptr<double> p= f();
+    std::cout << "p.use_count() = " << p.use_count() << std::endl;
+
+    /*std::shared_ptr<double>*/auto p1= std::make_shared<double>();
+    std::cout << "p1.use_count() = " << p1.use_count() << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
