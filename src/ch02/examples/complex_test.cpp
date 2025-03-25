df806f31283e42487e21d623839e5c07feec1dc0
Graham Strickland <grahamhstrickland@gmail.com>
2025-03-25 18:18:52 +0200

    Add more test cases for complex copy constructor

--- src/ch02/examples/complex_test.cpp
+++ src/ch02/examples/complex_test.cpp
@@ -39,5 +39,11 @@ int main(int argc, char *argv[]) {
   std::cout << "z1 = " << z1 << "\nz2 = " << z2 << "\nz3 = " << z3
             << "\nz4 = " << z4 << "\nz5 = " << z5 << '\n';
 
+  complex z6(3.0, 2.0),
+          z7(z6),
+          z8{z6};
+
+  std::cout << "z6 = " << z6 << "\nz7 = " << z7 << "\nz8 = " << z8 << '\n';
+
   return EXIT_SUCCESS;
 }
