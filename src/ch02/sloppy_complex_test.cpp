c286079681403b7dbf5cdaac657bd5457fd117fb
Graham Strickland <grahamhstrickland@gmail.com>
2025-04-21 14:19:35 +0200

    Add sloppy complex number example

---
+++ src/ch02/sloppy_complex_test.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+
+struct sloppy_complex {
+  double r, i;
+};
+
+int main() {
+  sloppy_complex z1{.r = 3.66, .i = 2.33},
+                 z2 = {.i = 1};
+  sloppy_complex z3{.i = 4.4, .r = 0.9};
+
+  return EXIT_SUCCESS;
+}
