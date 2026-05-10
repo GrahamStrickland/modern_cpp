a4a4bfe04ef040f624311d18378104db3798d1b9
Graham Strickland <grahamhstrickland@gmail.com>
2026-05-10 20:22:32 +0200

    Add remaining arithmetic operators to polynomial class

--- src/ch02/polynomial.cpp
+++ src/ch02/polynomial.cpp
@@ -7,23 +7,27 @@ std::ostream &operator<<(std::ostream &os, const polynomial &p) {
     return os;
 
   bool nonzero = false;
+  bool leading_minus = true;
 
   for (std::size_t i = 0; i <= p.degree; i++) {
     if (p.coeffs[i] != 0.0) {
       nonzero = true;
 
-      if (i == 0)
+      if (i == 0) {
         os << p.coeffs[i];
-      else {
-        os << (p.coeffs[i] > 0.0 ? " + " : " - ");
+        leading_minus = false;
+      } else {
+        os << (p.coeffs[i] > 0.0 ? " + " : (leading_minus ? "-" : " - "));
 
-        if (p.coeffs[i] != 1.0)
+        if (fabs(p.coeffs[i]) != 1.0)
           os << fabs(p.coeffs[i]);
 
         os << 'x';
 
         if (i > 1)
           os << '^' << i;
+
+        leading_minus = false;
       }
     }
   }
--- src/ch02/polynomial.h
+++ src/ch02/polynomial.h
@@ -72,4 +72,67 @@ inline polynomial operator+(const polynomial &p1, const polynomial &p2) {
 
   return polynomial(coeffs);
 }
+
+inline polynomial operator-(const polynomial &p1, const polynomial &p2) {
+  auto new_degree = std::max(p1.get_degree(), p2.get_degree());
+  std::vector<double> coeffs;
+  coeffs.reserve(new_degree);
+
+  for (std::size_t i = 0; i <= new_degree; i++) {
+    if (i <= p1.get_degree() && i <= p2.get_degree()) {
+      coeffs.emplace_back(p1.get_coeff(i) - p2.get_coeff(i));
+    } else if (i <= p1.get_degree()) {
+      coeffs.emplace_back(p1.get_coeff(i));
+    } else {
+      coeffs.emplace_back(-p2.get_coeff(i));
+    }
+  }
+
+  return polynomial(coeffs);
+}
+
+inline polynomial operator-(const polynomial &p) {
+  auto new_degree = p.get_degree();
+  std::vector<double> coeffs;
+  coeffs.reserve(new_degree);
+
+  for (std::size_t i = 0; i <= new_degree; i++) {
+    coeffs.emplace_back(-p.get_coeff(i));
+  }
+
+  return polynomial(coeffs);
+}
+
+inline polynomial operator*(const polynomial &p, double c) {
+  std::vector<double> coeffs;
+  coeffs.reserve(p.get_degree());
+
+  for (std::size_t i = 0; i <= p.get_degree(); i++) {
+    coeffs.emplace_back(p.get_coeff(i) * c);
+  }
+
+  return polynomial(coeffs);
+}
+
+inline polynomial operator*(double c, const polynomial &p) {
+  return operator*(p, c);
+}
+
+inline polynomial operator*(const polynomial &p1, const polynomial &p2) {
+  auto new_degree = p1.get_degree() * p2.get_degree() + 1;
+  std::vector<double> coeffs;
+  coeffs.reserve(new_degree);
+
+  for (std::size_t i = 0; i <= new_degree; i++) {
+    coeffs.emplace_back(0.0);
+  }
+
+  for (std::size_t i = 0; i <= p1.get_degree(); i++) {
+    for (std::size_t j = 0; j <= p2.get_degree(); j++) {
+      coeffs[i + j] += p1.get_coeff(i) * p2.get_coeff(j);
+    }
+  }
+
+  return polynomial(coeffs);
+}
 #endif // POLYNOMIAL_H
