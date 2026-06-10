d029e5321e07125db9a49372518079259b6c11d7
Graham Strickland <grahamhstrickland@gmail.com>
2026-06-10 18:11:10 +0200

    Add more max() template arg examples

--- src/ch03/max_test.cpp
+++ src/ch03/max_test.cpp
@@ -1,12 +1,10 @@
+// #include <complex>
 #include <cstdlib>
 #include <iostream>
+// #include <vector>
 
-template <typename T> T max(T a, T b) {
-  if (a > b)
-    return a;
-  else
-    return b;
-}
+// template <typename T, typename U> auto max(T a, U b) { return a > b ? a : b; }
+auto max(auto a, auto b) { return a > b ? a : b; }
 
 // template <typename T>
 // inline T max(T x, T y) {
@@ -25,6 +23,18 @@ int main(int argc, char *argv[]) {
             << '\n';
 
   std::cout << max<float>(8.1, 9.3) << '\n';
+
+  // std::complex<float> z(3, 2), c(4, 8);
+  // std::cout << "The maximum of c and z is " << ::max(c, z) << '\n';
+
+  // std::vector<std::complex<float>> v;
+  // std::sort(v.begin(), v.end());
+
+  unsigned u3 = 2;
+  int i = 3;
+
+  std::cout << "The maximum of u3 and i is " << max(int(u3), i) << '\n';
+  std::cout << "The maximum of u3 and i is " << max<int>(u3, i) << '\n';
 
   return EXIT_SUCCESS;
 }
