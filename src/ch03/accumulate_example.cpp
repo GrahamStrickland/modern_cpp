607c134accff06416a673bd4a5b8e235e3e59a34
Graham Strickland <grahamhstrickland@gmail.com>
2026-07-09 18:02:51 +0200

    Add more template accumulate examples

--- src/ch03/accumulate_example.cpp
+++ src/ch03/accumulate_example.cpp
@@ -8,12 +8,22 @@ template <typename T, unsigned N> T sum(const T (&array)[N]) {
   return sum;
 }
 
+template <typename T> inline T accumulate_array(T *a, T *a_end) {
+  T sum(0);
+  for (; a != a_end; ++a)
+    sum += *a;
+  return sum;
+}
+
 int main(int argc, char *argv[]) {
   int ai[] = {2, 4, 7};
   double ad[] = {2., 4.5, 7.};
 
   std::cout << "sum ai is " << sum(ai) << '\n';
   std::cout << "sum ad is " << sum(ad) << '\n';
+
+  std::cout << "sum ai is " << accumulate_array(ai, &ai[3]) << '\n';
+  std::cout << "sum ad is " << accumulate_array(ad, ad + 3) << '\n';
 
   return EXIT_SUCCESS;
 }
--- src/ch03/list.cpp
+++ src/ch03/list.cpp
@@ -8,6 +8,32 @@ template <typename T> struct list_entry {
   list_entry<T> *next;
 };
 
+template <typename T> struct list_iterator {
+  using value_type = T;
+  list_iterator(list_entry<T> *entry) : entry{entry} {}
+
+  T &operator*() { return entry->value; }
+
+  const T &operator*() const { return entry->value; }
+
+  list_iterator<T> &operator++() {
+    entry = entry->next;
+    return *this;
+  }
+
+  list_iterator<T> operator++(int) {
+    list_iterator<T> tmp(*this);
+    entry = entry->next;
+    return tmp;
+  }
+
+  bool operator!=(const list_iterator<T> &other) const {
+    return entry != other.entry;
+  }
+
+  list_entry<T> *entry;
+};
+
 template <typename T> struct list {
   list() : first{nullptr}, last{nullptr} {}
   ~list() {
@@ -21,14 +47,23 @@ template <typename T> struct list {
     last = (first ? last->next : first) = new list_entry<T>(x);
   }
   list_entry<T> *first, *last;
+
+  list_iterator<T> begin() { return list_iterator<T>(first); }
+  list_iterator<T> end() { return list_iterator<T>(0); }
 };
 
-template <typename T>
-T sum(const list<T> &l) {
+template <typename T> T sum(const list<T> &l) {
   T sum = 0;
   for (auto entry = l.first; entry != nullptr; entry = entry->next)
     sum += entry->value;
   return sum;
+}
+
+template <typename Iter, typename T>
+inline T accumulate(Iter it, Iter end, T init) {
+  for (; it != end; ++it)
+    init += *it;
+  return init;
 }
 
 int main(int argc, char *argv[]) {
@@ -38,6 +73,11 @@ int main(int argc, char *argv[]) {
   l.append(7.0f);
 
   std::cout << "sum l is " << sum(l) << '\n';
+
+  int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  std::cout << "array sum = " << accumulate(a, a + 10, 0.0) << '\n';
+  std::cout << "list sum = " << accumulate(l.begin(), l.end(), 0) << '\n';
 
   return EXIT_SUCCESS;
 }
