7c1953e29d1f0ca1d83da0b717fecc3b2a71d8b0
Graham Strickland <grahamhstrickland@gmail.com>
2026-07-19 11:57:44 +0200

    Add deduction guide to class template param deduction example

--- src/ch03/class_template_param_deduction.cpp
+++ src/ch03/class_template_param_deduction.cpp
@@ -1,12 +1,21 @@
 #include <algorithm>
 #include <cstdlib>
+// #include <iterator>
 #include <string>
 #include <tuple>
 
 template <typename Value> struct vec {
-  vec(std::initializer_list<Value> values) : s(values.size()), d(new Value[s]) {
+  vec(std::initializer_list<Value> values) : s(values.size()), d(new
+  Value[s]) {
     std::copy(begin(values), end(values), d);
   }
+  // template <class Iter>
+  // vec(Iter beg, Iter end)
+  //     ->vec<typename std::iterator_traits<Iter>::value_type>
+  //     : s(std::distance(beg, end)),
+  //     d(new Value[s]) {
+  //   std::copy(beg, end, d);
+  // }
 
   std::size_t s;
   Value *d;
@@ -19,6 +28,9 @@ int main(int argc, char *argv[]) {
   std::tuple t2(3, 3.2f, "text"s);
 
   vec v = {2.3, 3.4, 4.5, 5.6};
+
+  // int array[] = {3, 4, 5, 6};
+  // vec w(std::begin(array), std::end(array));
 
   return EXIT_SUCCESS;
 }
