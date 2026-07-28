f145d08988962d9e7bc5b7f3953e4f95b4b68b56
Graham Strickland <grahamhstrickland@gmail.com>
2026-07-28 18:36:07 +0200

    Add template specialization example

--- src/ch03/vector_template.cpp
+++ src/ch03/vector_template.cpp
@@ -1,9 +1,9 @@
 #include <cassert>
 #include <memory>
 
-template <typename T>
-class vector {
+template <typename T> class vector {
   void check_index(int i) { assert(i >= 0 && i < my_size); }
+
 public:
   explicit vector(int size) : my_size{size}, data{new T[my_size]} {}
 
@@ -21,4 +21,39 @@ public:
 private:
   int my_size;
   std::unique_ptr<T[]> data;
+};
+
+class vector_bool_proxy {
+public:
+  vector_bool_proxy(unsigned char &byte, int p)
+      : byte{byte}, mask{static_cast<unsigned char>(1 << p)} {}
+
+  operator bool() const { return byte & mask; }
+
+  vector_bool_proxy &operator=(bool b) {
+    if (b)
+      byte |= mask;
+    else
+      byte &= ~mask;
+    return *this;
+  }
+
+private:
+  unsigned char &byte;
+  unsigned char mask;
+};
+
+template <> class vector<bool> {
+public:
+  explicit vector(int size)
+      : my_size{size}, data{new unsigned char[(my_size + 7) / 8]} {}
+  vector() : my_size(0) {}
+
+  bool operator[](int i) const { return (data[i / 8] >> i % 8) & 1; }
+
+  vector_bool_proxy operator[](int i) { return {data[i / 8], i % 8}; }
+
+private:
+  int my_size;
+  std::unique_ptr<unsigned char[]> data;
 };
